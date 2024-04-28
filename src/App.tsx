effdc42ecb4d2356b095c92f79716c162e384ca0
Demonstrablee <unsuleed@gmail.com>
2024-04-28 18:41:58 -0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Link, Outlet } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +29,13 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+       <nav>
+        <Link to="/vitewebdemo/">Home</Link>
+        {" | "}
+        <Link to="/vitewebdemo/contact">Contact</Link>
+      </nav>
+
+      <Outlet />
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,29 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Home } from "./pages/Home.tsx";
+import { Contact } from "./pages/Contact.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/vitewebdemo/",
+    element: <App />,
+    children: [
+      {
+        path: "/vitewebdemo/",
+        element: <Home />,
+      },
+      {
+        path: "/vitewebdemo/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>,
 )
